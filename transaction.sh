c426a1c27ffe3c5aad3311252c289755ac19a5a4
devopscloudtrainer <119645417+devopscloudtrainer@users.noreply.github.com>
2025-12-16 08:51:22 +0530

    Ticket - 817122 transaction.sh for OTP Service

---
+++ transaction.sh
@@ -0,0 +1,5 @@
+* Stash: Stash is an temporary storage location for storing the changes.
+
+eg: feature/automation - middle of work - added/committed  - complete/rollback
+             STASH - feature/automation - changes into stash location - stash index - temporary storage location
+    urgent request - hotfix change - live env - 1 hour 
